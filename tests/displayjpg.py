a9a7d5bb753ff718cb7425c08ffd88aa772d3cfd
Jimbles <j.avery.10@ucl.ac.uk>
2016-04-19 10:25:45 +0100

    added scaling and centering of images in display_pics test

--- tests/displayjpg.py
+++ tests/displayjpg.py
@@ -7,16 +7,16 @@ import traceback
 
 now = "../samplepics/image"
 total_pics = 4 # number of pics  to be taken
-monitor_w = 800
-monitor_h = 450
-transform_x = 600 #how wide to scale the jpg when replaying
-transform_y = 450 #how high to scale the jpg when replaying
-offset_x = 100 #how far off to left corner to display photos
-offset_y = 0 #how far off to left corner to display photos
+monitor_w = 1024
+monitor_h = 600
+#transform_x = 1024 #how wide to scale the jpg when replaying
+#transform_y = 600 #how high to scale the jpg when replaying
+#offset_x = (monitor_w - transform_x) /2 #how far off to left corner to display photos
+#offset_y = 0 #how far off to left corner to display photos
 replay_delay = 1 # how much to wait in-between showing pics on-screen after taking
 replay_cycles = 1 # how many times to show each photo on-screen after taking
 
-fullscreen = 1
+fullscreen = 0
 
 
 def init_pygame():
@@ -40,7 +40,20 @@ def show_image(img_fname,screen = 0):
     if not screen:
         screen = init_pygame()
     img = pygame.image.load(img_fname)
-    img = pygame.transform.scale(img, (transform_x, transform_y))
+    img_h = img.get_height()
+    img_w = img.get_width()
+
+    if img_h == monitor_h and img_w == monitor_w:
+        img = pygame.transform.scale(img)
+        offset_x = 0
+        offset_y = 0
+    else:
+        y_scale_factor = monitor_h/ (1.0 * img_h) # force float
+        transform_y = int(img_h * y_scale_factor)
+        transform_x = int(img_w * y_scale_factor)
+        offset_x = (monitor_w - transform_x) / 2
+        offset_y = 0
+        img = pygame.transform.scale(img, (transform_x, transform_y))
     screen.blit(img, (offset_x, offset_y))
     pygame.display.flip()  # update the display
 
