2af7afd8b941f585bc6d39fe7f17f541f553cbcf
Jimbles <j.avery.10@ucl.ac.uk>
2016-04-23 16:04:17 +0100

    added all leds to gpio test

--- tests/gpio_simpletest.py
+++ tests/gpio_simpletest.py
@@ -6,23 +6,90 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-led =4
+led_1 = 2
+led_2 = 3
+led_3 = 4
+led_4 = 17
+led_5 = 27
+led_6 = 22
 
-GPIO.setup(led,GPIO.OUT)
 
-GPIO.output(led,1)
+GPIO.setup(led_1,GPIO.OUT)
+GPIO.setup(led_2,GPIO.OUT)
+GPIO.setup(led_3,GPIO.OUT)
+GPIO.setup(led_4,GPIO.OUT)
+GPIO.setup(led_5,GPIO.OUT)
+GPIO.setup(led_6,GPIO.OUT)
+
+GPIO.output(led_1,1)
+GPIO.output(led_2,1)
+GPIO.output(led_3,1)
+GPIO.output(led_4,1)
+GPIO.output(led_5,1)
+GPIO.output(led_6,1)
 
 sleep(1)
 
-GPIO.output(led,0)
+GPIO.output(led_1,0)
+GPIO.output(led_2,0)
+GPIO.output(led_3,0)
+GPIO.output(led_4,0)
+GPIO.output(led_5,0)
+GPIO.output(led_6,0)
 
 sleep(1)
 
-GPIO.output(led,1)
+GPIO.output(led_1,1)
+GPIO.output(led_2,1)
+GPIO.output(led_3,1)
+GPIO.output(led_4,1)
+GPIO.output(led_5,1)
+GPIO.output(led_6,1)
 
 sleep(1)
 
-GPIO.output(led,0)
+GPIO.output(led_1,0)
+GPIO.output(led_2,0)
+GPIO.output(led_3,0)
+GPIO.output(led_4,0)
+GPIO.output(led_5,0)
+GPIO.output(led_6,0)
+
+sleep(1)
+
+GPIO.output(led_1,1)
+GPIO.output(led_2,1)
+GPIO.output(led_3,1)
+GPIO.output(led_4,1)
+GPIO.output(led_5,1)
+GPIO.output(led_6,1)
+
+sleep(1)
+
+GPIO.output(led_1,0)
+GPIO.output(led_2,0)
+GPIO.output(led_3,0)
+GPIO.output(led_4,0)
+GPIO.output(led_5,0)
+GPIO.output(led_6,0)
+
+sleep(1)
+
+GPIO.output(led_1,1)
+GPIO.output(led_2,1)
+GPIO.output(led_3,1)
+GPIO.output(led_4,1)
+GPIO.output(led_5,1)
+GPIO.output(led_6,1)
+
+sleep(1)
+
+GPIO.output(led_1,0)
+GPIO.output(led_2,0)
+GPIO.output(led_3,0)
+GPIO.output(led_4,0)
+GPIO.output(led_5,0)
+GPIO.output(led_6,0)
 
 
 GPIO.cleanup()
